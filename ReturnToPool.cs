7786d43f967e5b352cbccb4c41f83fbfdeadadc9
LambChomp <dxdesjardins@gmail.com>
2024-01-13 19:15:31 -0500

    commented out code using external library

--- ReturnToPool.cs
+++ ReturnToPool.cs
@@ -6,15 +6,16 @@ namespace Lambchomp.Pool;
 
 public partial class ReturnToPool : Node
 {
-    [Export] private NodeEvent onWarpAfterFadeOut;
+    //[Export] private NodeEvent onWarpAfterFadeOut;
     private bool isAddedToPool = false;
 
     public override void _Notification(int what) {
         switch (what) {
             // Note: You can't QueueFree a pool object. You have to implement a system to ensure it's removed from a parent scene about to be QueueFreed.
-            case (int)NotificationReady:
-                onWarpAfterFadeOut?.AddListener(() => this.GetAncestor(2)?.RemoveChild(this.GetParent()));
-                break;
+            // Commented out code is an example of how to do this.
+            //case (int)NotificationReady:
+            //    onWarpAfterFadeOut?.AddListener(() => this.GetAncestor(2)?.RemoveChild(this.GetParent()));
+            //    break;
             case (int)NotificationExitTree:
                 isAddedToPool = PoolManager.ReleaseObject(this.GetParent());
                 if (!isAddedToPool)
